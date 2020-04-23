5b340172f9f9f62b9cbd68fc697688bc13a3fe30
Douglas-Reis <douglascouto39@gmail.com>
2020-04-22 22:56:45 -0300

    nao esta pronto

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:web');
+    }
+    public function index()
+    {
+        $people = Person::where('parent_id', null)->get();
+
+        return view('app.people.index', compact('categories'));
+    }
 }
