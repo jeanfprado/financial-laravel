53f9d6d7c566bd1827975f94d162d20eee91e353
Adailton Lucas <adasluc@hotmail.com>
2020-04-25 17:49:10 -0300

    migrate da tabela extracts

--- app/Extract.php
+++ app/Extract.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Extract extends Model
 {
-    //
+    protected $fillable = [
+        'account_id',
+        'person_id',
+        'dua_id',
+        'amount',
+        'category_id',
+        'type',
+        'settled',
+        'settled_at',
+        'user_id'
+    ];
 }
--- database/migrations/2020_04_24_030959_create_extracts_table.php
+++ database/migrations/2020_04_24_030959_create_extracts_table.php
@@ -15,6 +15,22 @@ class CreateExtractsTable extends Migration
     {
         Schema::create('extracts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->date('dua_at');
+            $table->decimal('amount',8,2);
+            $table->boolean('settled')->default(0);
+            $table->date('settled_at')->nullable;
+            $table->enum('type',['receive', 'expense']);
+
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('person_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+                        
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('user_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
